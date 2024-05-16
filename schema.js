e8912418e7a2ffd7fd8bf62e30c6cd1d262f199f
Asma-malica <115386995+Asma-malica@users.noreply.github.com>
2024-05-16 08:50:43 +0530

    updated

--- schema.js
+++ schema.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 async function connectionToDB(){
 
-    mongoose.connect('mongodb://127.0.0.1:27017/consultancy').then((data)=>{
+    // mongoose.connect('mongodb://127.0.0.1:27017/consultancy').then((data)=>{
+    mongoose.connect('mongodb+srv://asmamalicaj21cse:MSUaVxFedHQBIDrg@cluster0.thqsv8w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then((data)=>{
         console.log('connected to the DB')
     }).catch((err)=>{
         console.log('not connected')
